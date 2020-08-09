ddb547fd62f1f855baad65a3dc830db6044ee764
Ivan <ivan.zderadicka@gmail.com>
2020-08-09 16:40:21 +0200

    test: more in test_binary integration test

--- tests/test_binary.rs
+++ tests/test_binary.rs
@@ -1,12 +1,91 @@
-use anyhow::Result;
+#![allow(dead_code)]
+
+use anyhow::{Error, Result};
 use escargot::CargoBuild;
-use reqwest::{blocking::Client, StatusCode};
+use headers::HeaderValue;
+use reqwest::{blocking::Client, header::HeaderMap, StatusCode};
 use serde_json::Value;
 
 const BASE_URL: &str = "http://localhost:3000";
 
 fn make_url(path: &str) -> String {
     BASE_URL.to_string() + path
+}
+
+fn extract_value<'a>(mut v: &'a Value, path: &str) -> Result<&'a Value> {
+    let components = path.split('.');
+    for mut part in components {
+        let mut idx: Option<usize> = None;
+        if part.ends_with("]") {
+            let i = part
+                .find("[")
+                .ok_or_else(|| Error::msg("invalid array index"))?;
+            let n = part[i + 1..part.len() - 1].parse()?;
+            idx = Some(n);
+            part = &part[..i];
+        }
+        if !part.is_empty() {
+            if let Value::Object(o) = v {
+                v = o
+                    .get(part)
+                    .ok_or_else(|| Error::msg(format!("Invalid object key {}", part)))?
+            } else {
+                return Err(Error::msg("value is not an object"));
+            };
+        }
+
+        match (v, idx) {
+            (Value::Array(arr), Some(idx)) => {
+                v = arr.get(idx).ok_or_else(|| Error::msg("invalid index"))?
+            }
+            (_, Some(_)) => return Err(Error::msg("value is not an array")),
+            (_, None) => (),
+        }
+    }
+
+    Ok(v)
+}
+
+fn string_value(v: &Value) -> Result<&str> {
+    if let Value::String(ref s) = v {
+        Ok(s.as_str())
+    } else {
+        Err(Error::msg("value is not string"))
+    }
+}
+
+fn int_value(v: &Value) -> Result<i64> {
+    if let Value::Number(n) = v {
+        n.as_i64().ok_or_else(|| Error::msg("not integer value"))
+    } else {
+        Err(Error::msg("value is not integer"))
+    }
+}
+
+fn float_value(v: &Value) -> Result<f64> {
+    if let Value::Number(n) = v {
+        n.as_f64().ok_or_else(|| Error::msg("not float value"))
+    } else {
+        Err(Error::msg("value is not float"))
+    }
+}
+
+fn array_len(v: &Value) -> Result<usize> {
+    if let Value::Array(a) = v {
+        Ok(a.len())
+    } else {
+        Err(Error::msg("value is not array"))
+    }
+}
+
+macro_rules! assert_header {
+    ($resp:ident, $key: expr, $val: expr) => {
+        let v = $resp
+            .headers()
+            .get($key)
+            .ok_or_else(|| Error::msg(format!("header {} not found", $key)))?;
+        assert_eq!($val, v.to_str()?);
+    };
 }
 
 #[test]
@@ -53,25 +132,76 @@ fn test_binary() -> Result<()> {
     }
 
     assert_eq!(StatusCode::OK, resp.status());
-    let mime = resp.headers().get("Content-Type").unwrap();
-    assert_eq!("text/html", mime.to_str()?);
+    assert_header!(resp, "Content-Type", "text/html");
 
-    let collections: Value = client.get(&make_url("/collections")).send()?.json()?;
-    if let Value::Object(o) = collections {
-        let cols = o.get("names").unwrap();
-        if let Value::Array(v) = cols {
-            assert_eq!(1, v.len());
-            if let Value::String(s) = &v[0] {
-                assert_eq!("test_data", s)
-            } else {
-                panic!("names[0] is not string");
-            }
-        } else {
-            panic!("names are not array");
-        }
-    } else {
-        panic!("Collections are not JSON object")
-    }
+    let jg = |path| -> Result<Value> {
+        client
+            .get(&make_url(path))
+            .send()?
+            .json::<Value>()
+            .map_err(Error::new)
+    };
+    let collections = jg("/collections")?;
+    assert_eq!(1, extract_value(&collections, "names").and_then(array_len)?);
+    assert_eq!(
+        "test_data",
+        extract_value(&collections, "names[0]").and_then(string_value)?
+    );
+
+    let transcodings = jg("/transcodings")?;
+    assert_eq!(
+        64,
+        extract_value(&transcodings, "high.bitrate").and_then(int_value)?
+    );
+    assert_eq!(
+        "opus-in-ogg",
+        extract_value(&transcodings, "high.name").and_then(string_value)?
+    );
+
+    let root_folder = jg("/folder/")?;
+    assert_eq!(2, extract_value(&root_folder, "files").and_then(array_len)?);
+    assert_eq!(
+        2,
+        extract_value(&root_folder, "subfolders").and_then(array_len)?
+    );
+    assert_eq!(
+        "02-file.opus",
+        extract_value(&root_folder, "files[0].name").and_then(string_value)?
+    );
+    assert_eq!(
+        "audio/ogg",
+        extract_value(&root_folder, "files[0].mime").and_then(string_value)?
+    );
+    assert_eq!(
+        2,
+        extract_value(&root_folder, "files[0].meta.duration").and_then(int_value)?
+    );
+    assert_eq!(
+        48,
+        extract_value(&root_folder, "files[0].meta.bitrate").and_then(int_value)?
+    );
+
+    assert_eq!(
+        "audio/x-matroska",
+        extract_value(&root_folder, "files[1].mime").and_then(string_value)?
+    );
+
+    let res = client.get(&make_url("/audio/03-file.mka?trans=l")).send()?;
+    assert_eq!(StatusCode::OK, res.status());
+    assert_header!(res, "Content-Type", "audio/ogg");
+    assert_header!(res, "transfer-encoding", "chunked");
+    assert_header!(res, "x-transcode", "codec=opus-in-ogg; bitrate=32");
+
+    let mut range_headers = HeaderMap::new();
+    range_headers.insert("Range", HeaderValue::from_str("bytes=0-100")?);
+    let res = client
+        .get(&make_url("/audio/02-file.opus?trans=0"))
+        .headers(range_headers)
+        .send()?;
+    assert_header!(res, "Content-Type", "audio/ogg");
+    assert_eq!(StatusCode::PARTIAL_CONTENT, res.status());
+    assert_header!(res, "content-range", "bytes 0-100/12480");
+    assert_header!(res, "content-length", "101");
 
     #[cfg(not(unix))]
     proc.kill();
