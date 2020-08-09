74798a3507d87eb3b9be4ccd03007bde382cd7cb
Ivan <ivan.zderadicka@gmail.com>
2020-08-09 13:22:35 +0200

    fmt

--- tests/test_binary.rs
+++ tests/test_binary.rs
@@ -20,8 +20,13 @@ fn test_binary() -> Result<()> {
     eprintln!("Binary is at {:?}", bin.path());
 
     let mut cmd = bin.command();
-    cmd.args(&["--no-authentication", "--listen", "127.0.0.1:3000", "test_data"])
-        .env("RUST_LOG", "audioserve=debug");
+    cmd.args(&[
+        "--no-authentication",
+        "--listen",
+        "127.0.0.1:3000",
+        "test_data",
+    ])
+    .env("RUST_LOG", "audioserve=debug");
     let mut proc = cmd.spawn()?;
 
     eprintln!("Process is running with id {}", proc.id());
@@ -30,21 +35,21 @@ fn test_binary() -> Result<()> {
 
     let resp;
     let mut retries = 5;
-    
+
     loop {
         let r = client.get(&make_url("")).send();
-         match r {
-             Ok(r) => {
-            resp = r;
-            break;
+        match r {
+            Ok(r) => {
+                resp = r;
+                break;
+            }
+            Err(e) if retries > 0 => eprint!("Error connecting audioserve {}", e),
+            Err(e) if retries == 0 => panic!("Cannot connect to server, error: {}", e),
+            Err(_) => unreachable!(),
         }
-            Err(e) if retries> 0=> eprint!("Error connecting audioserve {}", e),
-            Err(e) if retries == 0 =>  panic!("Cannot connect to server, error: {}", e),
-            Err(_) => unreachable!()
-    }
-        
-        retries -=1; 
-        std::thread::sleep(std::time::Duration::from_secs(5-retries))
+
+        retries -= 1;
+        std::thread::sleep(std::time::Duration::from_secs(5 - retries))
     }
 
     assert_eq!(StatusCode::OK, resp.status());
@@ -56,7 +61,7 @@ fn test_binary() -> Result<()> {
         let cols = o.get("names").unwrap();
         if let Value::Array(v) = cols {
             assert_eq!(1, v.len());
-            if let Value::String(s) = &v[0]{
+            if let Value::String(s) = &v[0] {
                 assert_eq!("test_data", s)
             } else {
                 panic!("names[0] is not string");
@@ -64,7 +69,6 @@ fn test_binary() -> Result<()> {
         } else {
             panic!("names are not array");
         }
-
     } else {
         panic!("Collections are not JSON object")
     }
