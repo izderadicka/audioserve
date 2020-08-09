7033cc5dc7ea09c430b725350b2d0e31e8cb077b
Ivan <ivan.zderadicka@gmail.com>
2020-08-09 13:21:55 +0200

    integration test: fix - retries for first client connection

--- tests/test_binary.rs
+++ tests/test_binary.rs
@@ -20,7 +20,7 @@ fn test_binary() -> Result<()> {
     eprintln!("Binary is at {:?}", bin.path());
 
     let mut cmd = bin.command();
-    cmd.args(&["--no-authentication", "test_data"])
+    cmd.args(&["--no-authentication", "--listen", "127.0.0.1:3000", "test_data"])
         .env("RUST_LOG", "audioserve=debug");
     let mut proc = cmd.spawn()?;
 
@@ -28,13 +28,46 @@ fn test_binary() -> Result<()> {
 
     let client = Client::new();
 
-    let resp = client.get(&make_url("")).send()?;
+    let resp;
+    let mut retries = 5;
+    
+    loop {
+        let r = client.get(&make_url("")).send();
+         match r {
+             Ok(r) => {
+            resp = r;
+            break;
+        }
+            Err(e) if retries> 0=> eprint!("Error connecting audioserve {}", e),
+            Err(e) if retries == 0 =>  panic!("Cannot connect to server, error: {}", e),
+            Err(_) => unreachable!()
+    }
+        
+        retries -=1; 
+        std::thread::sleep(std::time::Duration::from_secs(5-retries))
+    }
 
     assert_eq!(StatusCode::OK, resp.status());
     let mime = resp.headers().get("Content-Type").unwrap();
     assert_eq!("text/html", mime.to_str()?);
 
-    let _collections: Value = client.get(&make_url("/collections")).send()?.json()?;
+    let collections: Value = client.get(&make_url("/collections")).send()?.json()?;
+    if let Value::Object(o) = collections {
+        let cols = o.get("names").unwrap();
+        if let Value::Array(v) = cols {
+            assert_eq!(1, v.len());
+            if let Value::String(s) = &v[0]{
+                assert_eq!("test_data", s)
+            } else {
+                panic!("names[0] is not string");
+            }
+        } else {
+            panic!("names are not array");
+        }
+
+    } else {
+        panic!("Collections are not JSON object")
+    }
 
     #[cfg(not(unix))]
     proc.kill();
