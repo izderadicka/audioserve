2c3bcca6c39bbd4c591a8ef931e3630914b03903
Ivan <ivan.zderadicka@gmail.com>
2023-12-30 11:09:46 +0100

    Add bench

---
+++ crates/leaky-cauldron/benches/bench.rs
@@ -0,0 +1,14 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use leaky_cauldron::Leaky;
+
+fn bench_leaky(b: &mut Criterion) {
+    let leaky = Leaky::new_with_params(1_000.0, 1_000);
+    b.bench_function("leaky", |b| {
+        b.iter(|| {
+            leaky.start_one().ok();
+        })
+    });
+}
+
+criterion_group!(benches, bench_leaky);
+criterion_main!(benches);
